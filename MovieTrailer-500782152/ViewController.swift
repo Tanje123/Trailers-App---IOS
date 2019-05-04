22f4ffa4b767b5de53e7c7995f26d00d4b309af3
Jasdeep <jasdeep@imacvanjasdeep.home>
2019-05-04 08:05:12 +0200

    Initial Commit

---
+++ MovieTrailer-500782152/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieTrailer-500782152
+//
+//  Created by Jasdeep on 04-05-19.
+//  Copyright © 2019 Tanveer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MovieTrailer-500782152Tests/MovieTrailer_500782152Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieTrailer_500782152Tests.swift
+//  MovieTrailer-500782152Tests
+//
+//  Created by Jasdeep on 04-05-19.
+//  Copyright © 2019 Tanveer. All rights reserved.
+//
+
+import XCTest
+@testable import MovieTrailer_500782152
+
+class MovieTrailer_500782152Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
